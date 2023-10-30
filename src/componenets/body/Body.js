62d741f2d8b89f6beb0209657b61e619a564bb65
ifoster01 <ifoster41901@gmail.com>
2023-10-30 18:58:05 -0400

    Updated body

--- src/componenets/body/Body.js
+++ src/componenets/body/Body.js
@@ -8,8 +8,22 @@ const Body = () => {
   return (
     <body className='webbody'>
         <div className='bodycontainer'>
-          <h1 className='bodyheader'>Hello!</h1>
-          <p className='bodytext'>I'm <div className='myname'>&nbsp;Isaac</div>, a software developer</p>
+          <div id='hello'>
+            <h1 className='bodyheader'>Hello!</h1>
+            <p className='bodytext'>
+              I'm <div className='myname'>&nbsp;Isaac</div>,
+            </p>
+            <p className='bodytext'>
+              a software developer
+            </p>
+            <p id='aboutme'>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            </p>
+          </div>
+          <div id='mypicture'>
+            img placeholder
+            {/* <img src='' alt='Isaac' className='myimage' /> */}
+          </div>
         </div>
         <Projects />
         <Resume />
--- src/componenets/body/projects/Projects.js
+++ src/componenets/body/projects/Projects.js
@@ -1,17 +1,27 @@
 import React from 'react';
+import './Projects.css';
 
 const Projects = () => {
   return (
     <body>
-        <ul>
-          <li>
-            Aixus Health
+        <ul className='projectslist'>
+          <li id='aixuscard'>
+            <h1 id='aixustitle'>
+              Aixus Health
+            </h1>
+            <div className='imgcontainer'>
+              <img id='aixusimg' src='https://i.imgur.com/2ZQ4Z5X.png' alt='Aixus Health'/>
+            </div>
           </li>
-          <li>
-            BestProfessorList
+          <li id='bplcard'>
+            <h1 id='bpltitle'>
+              BestProfessorList
+            </h1>
           </li>
-          <li>
-            BidOS
+          <li id='bidoscard'>
+            <h1 id='bidostitle'>
+              BidOS
+            </h1>
           </li>
         </ul>
     </body>
