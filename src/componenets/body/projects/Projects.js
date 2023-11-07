9c1c626161929f99159cfbb88acc948c465bcf49
ifoster01 <ifoster41901@gmail.com>
2023-11-07 15:30:14 -0500

    Updated images

--- src/componenets/body/projects/Projects.js
+++ src/componenets/body/projects/Projects.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './Projects.css';
 import Aixus from '../../../assets/aixus logo.png';
+import AixusTitle from '../../../assets/aixus title logo.png'
 import BidOS from '../../../assets/bidos logo.png';
+import BidOSScreen from '../../../assets/bidos screen.png'
 import BPL from '../../../assets/bpl logo.png';
 import Readee from '../../../assets/readee logo.png';
 
@@ -14,9 +16,9 @@ const Projects = () => {
       <ul className='projectslist'>
         <li className='projectcard' id='aixuscard'>
         <div className='projectwrapper'>
-            <h1 className='projecttitle'>
-              Aixus Health
-            </h1>
+            <div className='projecttitle'>
+              <img className='titleimg' src={AixusTitle} alt='Aixus Helth' />
+            </div>
             <p>
               Readee is a web application that allows users to search for books and add them to their reading list. The application uses the Google Books API to search for books and the user can add them to their reading list. The user can also remove books from their reading list. The application uses ReactJS for the front end and Redux for state management. The application also uses Firebase for authentication and database storage.
             </p>
@@ -27,12 +29,12 @@ const Projects = () => {
         </li>
         <li className='projectcard'>
           <div className='imgcontainer'>
-            <img className='projectimg' id='bidosimg' src={BidOS} alt='BidOS'/>
+            <img className='projectimg' id='bidosimg' src={BidOSScreen} alt='BidOS'/>
           </div>
           <div className='projectwrapper'>
-            <h1 className='projecttitle'>
-              BidOS
-            </h1>
+            <div className='projecttitle'>
+              <img className='titleimg' id='bidostitleimg' src={BidOS} alt='BidOS' />
+            </div>
             <p>
               Readee is a web application that allows users to search for books and add them to their reading list. The application uses the Google Books API to search for books and the user can add them to their reading list. The user can also remove books from their reading list. The application uses ReactJS for the front end and Redux for state management. The application also uses Firebase for authentication and database storage.
             </p>
