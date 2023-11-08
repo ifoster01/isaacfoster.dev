eca4e4561dbf20bdcb50a1623c8ee58ba04f1862
ifoster01 <ifoster@aixushealth.com>
2023-11-08 13:44:21 -0500

    Updated site

--- src/componenets/body/contact/Contact.js
+++ src/componenets/body/contact/Contact.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import './Contact.css';
 
 const Contact = () => {
   return (
-    <body>
-        hello
+    <body className='contactbody'>
+      <h1 className='contactheader'>Contact</h1>
+      <section className='contact'>
+        <div className='contactcontainer'>
+          <h1 className='contactblurb'>
+            Thanks for stopping by! You can reach out via email or LinkedIn. Also check out my GitHub to see my most recent projects and repositories.
+          </h1>
+          <ul className='contactpoints'>
+            <li className='email'>
+              ifoster41901@gmail.com
+            </li>
+            <li className='LinkedIn'>
+              <a href='https://www.linkedin.com/in/isaac-foster-2024/'>LinkedIn</a>
+            </li>
+            <li className='github'>
+              <a href='https://github.com/ifoster01'>GitHub</a>
+            </li>
+          </ul>
+        </div>
+      </section>
     </body>
   );
 };
--- src/componenets/body/resume/Resume.js
+++ src/componenets/body/resume/Resume.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import './Resume.css';
+import ResumePDF from '../../../assets/Isaac Foster Resume.pdf';
 
 const Resume = () => {
   return (
-    <body>
-        hello
+    <body className='resumebody'>
+      <h1 className='resumeheader'>Resume</h1>
+      <section className='resume'>
+        <div className='resumecontainer'>
+          <iframe src={ResumePDF} className='resumepdf'/>
+        </div>
+      </section>
     </body>
   );
 };
